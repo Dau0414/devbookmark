8bf2a40a385188090d990af54a97af12a73bb501
Dau0414 <khaunglachaw@gmail.com>
2026-04-06 19:32:03 +0900

    fix addbookmark

--- src/app/createPost/page.tsx
+++ src/app/createPost/page.tsx
@@ -1,6 +1,13 @@
 import { CreateBookmarkForm } from "@/src/features/myBook/components/create-bookmark-form"
+import { getSession } from "@/src/lib/get-session"
+import { signIn, signUp } from "@/src/path"
+import { redirect } from "next/navigation"
 
-const CreatePost = () => {
+const  CreatePost = async() => {
+  const session=await getSession()
+  if(!session){
+    redirect(signUp)
+  }
   return (
     <div className='container mx-auto px-4 py-8'>
       <CreateBookmarkForm />
